3b9aec3aa673dcb1aca1fcabd4c4995c091b90ec
mishgha <mishghazahid321@gmail.com>
2023-11-14 10:42:17 +0500

    My first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        //Generics Provides us with increased type safety
+        //Because it provides us with compile time checking
+        //Whereas the Object is the superclass of all other classes, and object reference can refer to any object.
+        //and that's why object does runtime checking 
+
+        System.out.println("hello there");
+    }
+}
